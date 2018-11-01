53a8ba07c2089f5756b4148e36fbec68ccb51bd0
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2018-11-01 10:44:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -50,25 +50,25 @@ $response .= "4. Kingsley Moghalu \n";
 else if($text == "2*1"){
 
 // Display information of Fela Durotoye
-$response .= "END Fela is a 46 years old Public speaker and business strategist of the part ANN(alliance of new nigeria) \n -BSc. Computer Science and Economics, and MBA/MSc.Business Strategy from Obafemi Awolowo University \n Look up the Mushin Makeover Project"; 
+$response .= "END Fela Durotoye is a 46 years old Public speaker and business strategist of the part ANN(alliance of new nigeria) \n -BSc. Computer Science and Economics, and MBA/MSc.Business Strategy from Obafemi Awolowo University \n Look up the Mushin Makeover Project"; 
 }
 
 else if($text == "2*2"){
 
 // Display information of Donald Duke
-$response .= "END Donald is a 56 years old of the party SDP(Social Democratic Party) \n -LLB from Ahmadu Bello University, \n LLM from the University of Pennsylvania \n -Former Commissioner for Finance and Planning \n -Former Governor of Cross River \n -Initiated the Tinapa Free Zone & Resort, and the Calabar Carnival" ; 
+$response .= "END Donald Duke is a 56 years old of the party SDP(Social Democratic Party) \n -LLB from Ahmadu Bello University, \n LLM from the University of Pennsylvania \n -Former Commissioner for Finance and Planning \n -Former Governor of Cross River \n -Initiated the Tinapa Free Zone & Resort, and the Calabar Carnival" ; 
 }
 
 else if($text == "2*3"){
 
-// Display information of Donald Duke
+// Display information of Buhari
 $response .= "END Buhari is a 76 years old of party APC(All Progessive Congress)\n -LLB from Ahmadu Bello University, \n LLM from the University of Pennsylvania \n -Former Commissioner for Finance and Planning \n -Former Governor of Cross River \n -Initiated the Tinapa Free Zone & Resort, and the Calabar Carnival" ; 
 }
 
 else if($text == "2*4"){
 
 // Display information of Kingsley Moghalu
-$response .= "END Kingsley is a 55 years old of YPP(Young People's Party)\n Professor of Practice in International Business and Public Policy at Tufts University\n
+$response .= "END Kingsley Moghalu is a 55 years old of YPP(Young People's Party)\n Professor of Practice in International Business and Public Policy at Tufts University\n
 -Attended UNN and LSE (PhD)\n
 -President of the Institute of Governance and Economic Transformation\n
 -Former United Nations official\n
